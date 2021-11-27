1dc2fe98570e3af699cbb776eff0af7591d1459f
Urszula Jarych <urszula.jarych@gmail.com>
2021-11-27 21:08:17 +0100

    useState with updating objects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, SetCounter] = useState(0);
+  const [order, SetOrder] = useState({ count: 0, dish: "dumplings" });
+  const count = order.count;
+  const dish = order.dish;
+
+  function incrementOrder() {
+    SetOrder((prevOrder) => {
+      return { ...prevOrder, count: prevOrder.count + 1 };
+    });
+  }
+
+  function decrementtOrder() {
+    SetOrder((prevOrder) => {
+      return { ...prevOrder, count: prevOrder.count - 1 };
+    });
+  }
+
   return (
-    <div className="App">
-      <button onClick={() => SetCounter((counter) => counter - 1)}> - </button>
-      <span>{counter}</span>
-      <button onClick={() => SetCounter((counter) => counter + 1)}> + </button>
+    <div className="container">
+      <h3>{count > 0 ? `How many ${dish} ?` : `C'mon order some dumplings`}</h3>
+      <>
+        <button onClick={decrementtOrder}> - </button>
+        <span>
+          {count} {dish}
+        </span>
+        <button onClick={incrementOrder}> + </button>
+      </>
     </div>
   );
 }
